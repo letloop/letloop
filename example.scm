04deb642dacb27d35f1d0f5dc7fecc434df0830b
Amir <amirouche@hyper.dev>
2023-04-23 16:25:37 +0200

    Add program example.scm.

---
+++ example.scm
@@ -0,0 +1,6 @@
+(import (chezscheme))
+
+(display "Expected output is what remains toward a super star number: ")
+(display (- (+ 2006 1337) (string->number (cadr (command-line)))))
+(newline)
+(flush-output-port)
