6006d5601c7f7fd78c1e8dc9bd7dc61fa44b97c3
Amir <amirouche@hyper.dev>
2023-06-22 18:51:39 +0200

    make-check.sh: print a message at the end...
    ... to cheer up the user, or not.

--- make-check.sh
+++ make-check.sh
@@ -32,8 +32,10 @@ EXPECTED="b4907e17e91609ea83394b6079794395"
 GIVEN=$($LETLOOP check --dry-run checks/check/ checks/check/check-success.scm | md5sum)
 if [ "x$GIVEN" = "x$EXPECTED" ] 
 then
+    echo "success!"
     exit 0
 else
+    echo "failure..."
     exit 1
 fi
 
