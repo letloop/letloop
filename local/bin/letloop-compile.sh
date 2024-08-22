ce018f25e787e8144310762d8da503cfcdf813eb
Amirouche Amazigh BOUBEKKI <amirouche@hyper.dev>
2024-08-22 23:26:11 +0200

    replace .../bin/letloop with .../lib/letloop.boot... and fix indentation!

--- local/bin/letloop-compile.sh
+++ local/bin/letloop-compile.sh
@@ -6,63 +6,61 @@ set -xe
 LETLOOP_ROOT=$1
 
 if [ -z ${LETLOOP_ROOT+x} ]; then
-    echo "LETLOOP_ROOT is unset"
-    exit 42
+  echo "LETLOOP_ROOT is unset"
+  exit 42
 fi
 
 LETLOOP_PREFIX=$2
 
 if [ -z ${LETLOOP_PREFIX+x} ]; then
-    echo "LETLOOP_PREFIX is unset"
-    exit 42
+  echo "LETLOOP_PREFIX is unset"
+  exit 42
 fi
 
 LETLOOP_FLAVOR=$3
 
 if [ -z ${LETLOOP_FLAVOR+x} ]; then
-    echo "LETLOOP_FLAVOR is unset"
-    exit 42
+  echo "LETLOOP_FLAVOR is unset"
+  exit 42
 fi
 
 set -xe
 
 if [ "x$LETLOOP_FLAVOR" = "xcisco" ]; then
-    rm -rf $LETLOOP_PREFIX/src/chez/cisco
-    mkdir -p $LETLOOP_PREFIX/src/chez/cisco
+  rm -rf $LETLOOP_PREFIX/src/chez/cisco
+  mkdir -p $LETLOOP_PREFIX/src/chez/cisco
 
-    git clone --filter=blob:none --depth=1 https://github.com/cisco/ChezScheme/ $LETLOOP_PREFIX/src/chez/cisco/
+  git clone --filter=blob:none --depth=1 https://github.com/cisco/ChezScheme/ $LETLOOP_PREFIX/src/chez/cisco/
 
-    PREFIX=$LETLOOP_PREFIX
-    mkdir -p $PREFIX
+  PREFIX=$LETLOOP_PREFIX
+  mkdir -p $PREFIX
 
-    cd $LETLOOP_PREFIX/src/chez/cisco/
+  cd $LETLOOP_PREFIX/src/chez/cisco/
 
-    ./configure --threads --disable-x11 --disable-curses --installprefix=$PREFIX --kernelobj > /dev/null
-    make -j $(nproc) > /dev/null
-    make install > /dev/null
+  ./configure --threads --disable-x11 --disable-curses --installprefix=$PREFIX --kernelobj >/dev/null
+  make -j $(nproc) >/dev/null
+  make install >/dev/null
 else
-    # Consider racket flavor
-    rm -rf $LETLOOP_PREFIX/src/chez/racket
-    mkdir -p $LETLOOP_PREFIX/src/chez/racket
+  # Consider racket flavor
+  rm -rf $LETLOOP_PREFIX/src/chez/racket
+  mkdir -p $LETLOOP_PREFIX/src/chez/racket
 
-    git clone --depth=1 https://github.com/racket/ChezScheme/ $LETLOOP_PREFIX/src/chez/racket/
+  git clone --depth=1 https://github.com/racket/ChezScheme/ $LETLOOP_PREFIX/src/chez/racket/
 
-    PREFIX=$LETLOOP_PREFIX/
-    mkdir -p $PREFIX
+  PREFIX=$LETLOOP_PREFIX/
+  mkdir -p $PREFIX
 
-    cd $LETLOOP_PREFIX/src/chez/racket/
+  cd $LETLOOP_PREFIX/src/chez/racket/
 
-    ./configure --kernelobj --threads --disable-x11 --disable-curses --installprefix=$PREFIX > /dev/null
-    make -j $(nproc) > /dev/null
-    make install > /dev/null
+  ./configure --kernelobj --threads --disable-x11 --disable-curses --installprefix=$PREFIX >/dev/null
+  make -j $(nproc) >/dev/null
+  make install >/dev/null
 fi
 
 cp $LETLOOP_ROOT/letloop.md .
 cp $LETLOOP_ROOT/letloop.nfo .
-cp $LETLOOP_ROOT/main.c .
 
 cd $LETLOOP_ROOT && git config --global --add safe.directory /mnt
 
 make SCHEME=$PREFIX/bin/scheme
-mv local/bin/letloop $PREFIX/bin/letloop
 LD_LIBRARY_PATH=$LETLOOP_ROOT/local/lib ./venv $(pwd)/local make check
